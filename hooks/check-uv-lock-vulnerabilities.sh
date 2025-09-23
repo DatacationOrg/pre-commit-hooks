d7939b36afdc090be83892029de29ac0a51e11ee
Erik Wouters <6179932+EWouters@users.noreply.github.com>
2025-09-23 14:18:34 +0200

    Create uv.lock vulnerability check pre-commit hook
    Create uv.lock vulnerability check pre-commit hook

---
+++ hooks/check-uv-lock-vulnerabilities.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Name of temporary requirements file
+REQ_FILE="requirements-check-uv-lock-vulnerabilities.txt"
+
+# Create a temporary requirements file
+uv export --format=requirements-txt > "$REQ_FILE"
+
+# Remove -e . if it exists
+if [[ -f "$REQ_FILE" ]]; then
+    sed -i '/^-e \.$/d' "$REQ_FILE"
+fi
+
+# Run pip-audit
+pip-audit -r "$REQ_FILE" --progress-spinner on
+
+# Remove temporary requirements file
+rm "$REQ_FILE"
